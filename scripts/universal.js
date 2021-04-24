279026a17980eb42075280a6d4da997fe2577feb
joseldri <joseldri@indiana.edu>
2021-04-24 15:24:02 -0700

    index-.html update
    added cards and filter functionality, renamed some classes

--- scripts/universal.js
+++ scripts/universal.js
@@ -29,3 +29,23 @@ function itemFilter(inputLocation, listULID) {
     }
   }
 }
+
+function cardFilter() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("search-box_bar");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("search-card-area");
+  li = ul.getElementsByTagName('div');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
