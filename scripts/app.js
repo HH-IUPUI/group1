180c0088c5f628226e52d019572524db6f00d2a2
Shannon Schrauwen <63632791+ShanSchrauwen@users.noreply.github.com>
2021-04-25 19:10:15 +0300

    Making country pages work with new template Joseph created
    Addition of q&a box
    made q&a box work and look nicely
    
    Changes in build:css script to concat the new style pages
    
    ERR in indexStyles.css nav-link fixed (rgba needs 4 values, needed to set alpha)
    
    deleted unnecessary pages and style sheets
    
    TODO: need to fix the styles pages so we have one set font and font size. Currently if we use the minimized/concated version of style pages we will have weird styling throughout the pages. need to decide and sort that out.

--- scripts/app.js
+++
@@ -1,7 +0,0 @@
-document
-  .getElementsByID("body")
-  .addEventListener("click", function(){alert("hello")});
-
-function closeNav() {
- document.getElementsByID("navi-toggle").checked = false;
-}
